156f0f8f3fc63d66a6202f6c0fb9932131454799
Michal Karm Babacek <mbabacek@redhat.com>
2012-08-28 18:38:37 +0200

    BugFix and some output

--- src/sf_uploader.py
+++ src/sf_uploader.py
@@ -25,12 +25,19 @@ class Processor:
 if __name__ == '__main__':
     processor = Processor()
     processor.getJobs()
+    number_of_jobs = len(processor.job_elements)
+    number_of_jobs_done = 0.
     for job in processor.job_elements:
+        number_of_jobs_done += 1
+        progress = number_of_jobs_done / (float(number_of_jobs) / 100.)
+        message = "Progress: %d %%, Processing job %s ..." % (progress, job.tag)
+        sys.stdout.write(message)
         project = job.find("project")
         project = ET.tostring(project, encoding=properties.ENCODING)
         opener = urllib2.build_opener()
         opener.add_handler(urllib2_kerberos.HTTPKerberosAuthHandler())
         req = urllib2.Request(properties.BASE_URL + properties.CONFIG_CONTEXT + job.tag + properties.CONFIX_XML, data = project, headers = { "Content-type" : "text/xml; charset="+properties.ENCODING})
         response = opener.open(req)
-        print response.code
+        print " DONE" if response.code == 200 else " ERROR code: %d" % response.code
+    print "%d jobs processed. All the xml configurations were uploaded from file %s to your Jenkins :-)" % (number_of_jobs, properties.TARGET_FILE)
 
