d417b59a070c726219977b5c4aaf16406a8c2bab
Michal Karm Babacek <mbabacek@redhat.com>
2012-08-28 15:50:34 +0200

    Downloads XML configurations and saves them in a file.

--- src/properties.py
+++ src/properties.py
@@ -1,9 +1,12 @@
 '''
 Created on Aug 28, 2012
 
-@author: Michal Karm Babacek
+@author: Michal Karm Babacek <mbabacek@redhat.com>
 '''
-
+BASE_URL = "https://jenkins.mw.lab.eng.bos.redhat.com"
 #URL = "https://jenkins.mw.lab.eng.bos.redhat.com/hudson/view/mod_cluster-QE/view/mod_cluster-QE-eap-5x/"
-URL = "https://jenkins.mw.lab.eng.bos.redhat.com/hudson/view/mod_cluster-QE/view/mod_cluster-QE-eap-6x/"
+URL = BASE_URL+"/hudson/view/mod_cluster-QE/view/mod_cluster-QE-eap-6x/"
+CONFIG_CONTEXT = "/hudson/job/"
+CONFIX_XML = "/config.xml"
 REGEXP = ".*<td><a href=\"job\/([^\/]*)\/.*"
+TARGET_FILE = "./test.xml"
--- src/sf_downloader.py
+++ src/sf_downloader.py
@@ -3,15 +3,16 @@
 '''
 Created on Aug 28, 2012
 
-@author: Michal Karm Babacek
+@author: Michal Karm Babacek <mbabacek@redhat.com>
 '''
+import xml.etree.ElementTree as ET
 import urllib2
 import urllib2_kerberos
 import properties
 import re
+import time
 
-
-class PageProcessor:
+class Processor:
     """
     Processes a raw html page from the Jenkins URL (defined in the properties file).
     """
@@ -26,10 +27,36 @@ class PageProcessor:
             result = regex.search(line)
             if result != None:
                 self.jobs.append(result.groups()[0])
+                
+    def getJobXML(self, job):
+        opener = urllib2.build_opener()
+        opener.add_handler(urllib2_kerberos.HTTPKerberosAuthHandler())
+        resp = opener.open(properties.BASE_URL+properties.CONFIG_CONTEXT+job+properties.CONFIX_XML)
+        return resp.read()
 
 if __name__ == '__main__':
-    pp = PageProcessor()
-    pp.getJobs()
-    print pp.jobs
-    
-
+    start = time.clock()
+    targetFile = open(properties.TARGET_FILE, 'w')
+    agregatedRoot = ET.Element('rootview')
+    processor = Processor()
+    processor.getJobs()
+    processor.jobs = ['eap-6x-mod_cluster-benchmark-mod_cluster-clusterlab', 'eap-6x-mod_cluster-benchmark-mod_cluster-mix-version']
+    print processor.jobs
+    for job in processor.jobs:
+        print 'Processing job %s ...' % job
+        xml = processor.getJobXML(job)
+        '''
+        Meh, no XML processing will be necessary, we just save it...
+        root = ET.fromstring(xml)
+        for child in root:
+            if child.tag == "description":
+                print child.tag, child.attrib, child.text
+        '''
+        jobXMLroot = ET.fromstring(xml)
+        jobName = ET.Element(processor.jobs[0])
+        jobName.append(jobXMLroot)
+        agregatedRoot.append(jobName)
+        print 'DONE\n'
+    ET.ElementTree(agregatedRoot).write(targetFile)
+    targetFile.close()
+    print "Processing %d jobs took %f seconds. All the xml configurations are in %s file." % (len(processor.jobs), (time.clock() - start), properties.TARGET_FILE)
