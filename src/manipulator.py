a7893150001262e9c08bccf5f59e9c34c382519d
Michal Karm Babacek <mbabacek@redhat.com>
2012-09-07 12:24:29 +0200

    Some more features for my own good...

--- src/manipulator.py
+++ src/manipulator.py
@@ -18,6 +18,8 @@ class Manipulator:
         elements as well as edit some text.
         """
         element = self.addIRCNotification(element)
+        element = self.removeObsoleteShellTask(element)
+        element = self.switchSmartFrogInstance(element)
         return element
 
     def addIRCNotification(self, element):
@@ -30,5 +32,24 @@ class Manipulator:
             publishers.remove(old_irc_publisher)
         irc_publisher_root = ET.fromstring(properties.IRC_PUBLISHER_XML)
         publishers.append(irc_publisher_root)
+        return element
+
+    def removeObsoleteShellTask(self, element):
+        """
+        We remove an obsolete ShellTask
+        """
+        builders = element.find("project").find("builders")
+        shell_task = builders.find("hudson.tasks.Shell")
+        if shell_task != None:
+            builders.remove(shell_task)
+        return element
+
+    def switchSmartFrogInstance(self, element):
+        """
+        Meh, we kinda swap SmartFrog instances...
+        """
+        smartfrog_name = element.find("project").find("builders").find("builder.smartfrog.SmartFrogBuilder").find("smartFrogName")
+        #if smartfrog_name.text is "SmartFrog 3.17 (DEBUG) mbabacek_fix":
+        smartfrog_name.text = "SmartFrog 3.17 (DEBUG) mbabacek_playground"
         return element
 
