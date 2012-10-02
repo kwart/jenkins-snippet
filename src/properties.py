96b680da2897bba0a4a2da4c2e349b48f0b94b71
Michal Karm Babacek <mbabacek@redhat.com>
2012-10-02 12:01:15 +0200

    AS update

--- src/properties.py
+++ src/properties.py
@@ -8,8 +8,8 @@ Created on Aug 28, 2012
 URLs and contexts
 """
 BASE_URL = "https://jenkins.mw.lab.eng.bos.redhat.com"
-#URL = BASE_URL+"/hudson/view/mod_cluster-QE/view/mod_cluster-QE-eap-5x/"
-URL = BASE_URL+"/hudson/view/mod_cluster-QE/view/mod_cluster-QE-eap-6x/"
+URL = BASE_URL+"/hudson/view/mod_cluster-QE/view/mod_cluster-QE-eap-5x/"
+#URL = BASE_URL+"/hudson/view/mod_cluster-QE/view/mod_cluster-QE-eap-6x/"
 CONFIG_CONTEXT = "/hudson/job/"
 CONFIX_XML = "/config.xml"
 REGEXP = ".*<td><a href=\"job\/([^\/]*)\/\">.*"
@@ -17,8 +17,8 @@ REGEXP = ".*<td><a href=\"job\/([^\/]*)\/\">.*"
 """
 Target files for storing our jobs
 """
-#TARGET_FILE = "./xmls/EAP5.xml"
-TARGET_FILE = "./xmls/EAP6.xml"
+TARGET_FILE = "./xmls/EAP5.xml"
+#TARGET_FILE = "./xmls/EAP6.xml"
 ENCODING = "utf-8"
 LEAVE_UNCHANGED = False
 
