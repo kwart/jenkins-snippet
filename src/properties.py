3249054139b2f7a63c71fdc33dd5f819df41a44c
Michal Karm Babacek <mbabacek@redhat.com>
2013-04-03 13:45:10 +0200

    mod_cluster view moved

--- src/properties.py
+++ src/properties.py
@@ -8,8 +8,8 @@ Created on Aug 28, 2012
 URLs and contexts
 """
 BASE_URL = "https://jenkins.mw.lab.eng.bos.redhat.com"
-#URL = BASE_URL+"/hudson/view/mod_cluster-QE/view/mod_cluster-QE-eap-5x/"
-URL = BASE_URL+"/hudson/view/mod_cluster-QE/view/mod_cluster-QE-eap-6x/"
+#URL = BASE_URL+"/hudson/view/mod_cluster/view/mod_cluster-QE/view/mod_cluster-QE-eap-5x/"
+URL = BASE_URL+"/hudson/view/mod_cluster/view/mod_cluster-QE/view/mod_cluster-QE-eap-6x/"
 CONFIG_CONTEXT = "/hudson/job/"
 CONFIX_XML = "/config.xml"
 REGEXP = ".*<td><a href=\"job\/([^\/]*)\/.*"
